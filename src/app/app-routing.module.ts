9aa4ef5b2f2e56a152af76cc97f00b514472644d
Adil-K <adil.khan@euri.com>
2020-10-13 17:37:34 +0200

    feat: added access denied page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: 'events',
     loadChildren: () =>
       import('./events/events.module').then((m) => m.EventsPageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'events/new',
@@ -18,11 +20,20 @@ const routes: Routes = [
       import('./event-create/event-create.module').then(
         (m) => m.EventCreatePageModule
       ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'event/:id',
     loadChildren: () =>
       import('./event/event.module').then((m) => m.EventPageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'access-denied',
+    loadChildren: () =>
+      import('./pages/access-denied/access-denied.module').then(
+        (m) => m.AccessDeniedPageModule
+      ),
   },
 ];
 
