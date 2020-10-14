eeeeb5a12a2b86732a4041b180c8736e74122ec2
Adil-K <adil.khan@euri.com>
2020-10-14 16:52:13 +0200

    feat: added auth settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: '19b9c004-a5de-4508-b316-a86b0612eaa5',
+  authority:
+    'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/',
+  redirectUrl: 'https://ashy-bay-024b4ff03.azurestaticapps.net',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientId: '19b9c004-a5de-4508-b316-a86b0612eaa5',
+  authority:
+    'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/',
+  redirectUrl: 'http://localhost:8100',
 };
 
 /*
