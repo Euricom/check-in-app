165f39be0ff099fd42fb162a43438445e86a9209
Adil-K <adil.khan@euri.com>
2020-10-21 09:37:47 +0200

    feat: get user at events page

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -1,7 +1,9 @@
+import { AuthService } from './../shared/services/auth.service';
 import { EventService } from './../shared/services/event.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../shared/models/event.model';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-events',
@@ -9,12 +11,24 @@ import { Event } from '../shared/models/event.model';
   styleUrls: ['./events.page.scss'],
 })
 export class EventsPage implements OnInit {
-  constructor(private eventService: EventService, private router: Router) {}
+  constructor(
+    private eventService: EventService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   events = Array<Event>();
+  user: User;
+  subscriptions: Array<string>;
 
   ngOnInit() {
     this.populateList();
+    this.getUser();
+  }
+
+  async getUser() {
+    this.user = await this.authService.getUser();
+    console.log(this.user);
   }
 
   populateList(): void {
@@ -22,6 +36,8 @@ export class EventsPage implements OnInit {
       this.events = res;
     });
   }
+
+  setUserSubscriptions(userId, event): void {}
 
   goDetail(id): void {
     this.router.navigate(['event', id]);
@@ -32,8 +48,7 @@ export class EventsPage implements OnInit {
     this.populateList();
   }
 
-  createEvent() {
+  createEvent(): void {
     this.router.navigateByUrl('/events/new');
-    this.populateList();
   }
 }
