38dfe25cbaa0c35f79debe198c0a29ddd503de68
Adil-K <adil.khan@euri.com>
2020-10-21 09:36:15 +0200

    feat: auth service updated to get user for ms graph

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,26 +1,77 @@
+import { Client } from '@microsoft/microsoft-graph-client';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
-import { Observable, of } from 'rxjs';
+import { MsalService } from '@azure/msal-angular';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  public authenticated: boolean;
+  public user: User;
 
-  isLoggedIn(): boolean {
-    return true;
+  constructor(
+    private msalService: MsalService,
+    private userService: UserService
+  ) {
+    this.authenticated = this.msalService.getAccount() != null;
+    this.getUser().then((user) => (this.user = user));
   }
 
-  getActiveUser(userRole?: string): Observable<User> {
-    return of({
-      id: 5,
-      firstName: 'Adil',
-      lastName: 'Khan',
-      phoneNumber: '',
-      email: '',
-      checkedIn: [36, 5],
-      role: userRole || 'Admin',
+  signOut() {
+    this.msalService.logout();
+    this.authenticated = false;
+    this.user = null;
+  }
+
+  async getAccessToken(): Promise<any> {
+    const result = await this.msalService
+      .acquireTokenSilent({ scopes: ['user.read'] })
+      .catch((error) => {
+        console.log('Get token failed', JSON.stringify(error));
+      });
+    return result;
+  }
+
+  async getUser(): Promise<User> {
+    if (!this.authenticated) {
+      return null;
+    }
+
+    const graphClient = Client.init({
+      // Init graph client
+      authProvider: async (done) => {
+        const token = await this.getAccessToken().catch((error) => {
+          done(error, null);
+        });
+        if (token) {
+          done(null, token.accessToken);
+        } else {
+          done('Could not get access token', null);
+        }
+      },
     });
+
+    const graphUser = await graphClient.api('/me').get();
+
+    const user = new User({
+      _id: graphUser.id,
+      firstName: graphUser.givenName,
+      lastName: graphUser.surname,
+      email: graphUser.mail,
+      phoneNumber: graphUser.mobilePhone,
+      checkedIn: [],
+      role: '',
+    });
+
+    // Add user to db if not exists
+    this.userService.getById(graphUser.id).subscribe((result) => {
+      if (Object.keys(result).length === 0) {
+        this.userService.create(user);
+      }
+    });
+
+    return user;
   }
 }
