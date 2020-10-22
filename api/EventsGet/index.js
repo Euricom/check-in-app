d3e553d7a4c891eb6d6ad6727a1f735b715bea42
Adil-K <adil.khan@euri.com>
2020-10-22 09:45:44 +0200

    api: get user subs with events

--- api/EventsGet/index.js
+++ api/EventsGet/index.js
@@ -5,10 +5,9 @@ module.exports = async function (context, req) {
 
   try {
     const database = await loadDB();
-    let result = [];
     let filtered = [];
 
-    let subscriptions = await database
+    let eventsAndUserSubs = await database
       .collection('events')
       .aggregate([
         {
@@ -24,69 +23,37 @@ module.exports = async function (context, req) {
             as: 'userEvents',
           },
         },
-
-        { $unwind: { path: '$userEvents', preserveNullAndEmptyArrays: true } },
-
         {
           $addFields: {
             subscribed: '$userEvents.subscribed.id',
-            // isSubbed: {
-            //   $cond: ['$userEvents.subscribed.id === $eventId', true, false],
-            // },
           },
         },
-
         {
           $project: {
             userEvents: 0,
             userId: 0,
-            // isSubbed: { $cond: ['subscribed === eventId', true, false] },
           },
         },
-
         { $unwind: { path: '$subscribed', preserveNullAndEmptyArrays: true } },
       ])
       .toArray();
 
-    let events = await database.collection('events').find().toArray();
-
-    // context.log(subscriptions[0].subscribed);
-
-    // if (subscriptions[0].subscribed.lenght === 0 || null) {
-    //   this.result = events;
-    // }
-
-    subscriptions.forEach((item) => {
-      if (item.eventId === item.subscribed) {
+    eventsAndUserSubs.forEach((item) => {
+      if (
+        item.subscribed &&
+        item.eventId ===
+          item.subscribed.find((res) => {
+            console.log(res);
+            return res === item.eventId;
+          })
+      ) {
         filtered.push({ ...item, subscribed: true });
+      } else {
+        filtered.push({ ...item, subscribed: false });
       }
     });
 
-    context.log(filtered);
-
-    // if (!subscriptions[0].id) {
-    //   result = events;
-    // } else {
-    //   subscriptions.forEach((item) => {
-    //     events.forEach((res) => {
-    //       if (res.eventId === item.id) {
-    //         result.push({ ...res, checkedIn: item.checkedIn });
-    //       } else result.push({ ...res });
-    //     });
-    //   });
-    // }
-
-    // subscriptions.forEach((item) => {
-    //   context.log(item);
-    //   events.forEach((res) => {
-    //     context.log(res.eventId, item.id, res.eventId === item.id);
-    //     if (res.eventId === item.id) {
-    //       result.push({ ...res, subscribed: true });
-    //     }
-    //   });
-    // });
-
-    context.res = { body: subscriptions };
+    context.res = { body: filtered };
   } catch (error) {
     context.log(`Error code: ${error.code} message: ${error.message}`);
     context.res = {
