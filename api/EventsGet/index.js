1f70554c7d99632160c29e154954fbe820ca942e
Adil-K <adil.khan@euri.com>
2020-11-06 10:05:41 +0100

    api: add subscription counts to get all events

--- api/EventsGet/index.js
+++ api/EventsGet/index.js
@@ -6,6 +6,7 @@ module.exports = async function (context, req) {
   try {
     const database = await loadDB();
     let filtered = [];
+    let filteredAndSubCount = [];
 
     let eventsAndUserSubs = await database
       .collection('events')
@@ -38,7 +39,41 @@ module.exports = async function (context, req) {
       ])
       .toArray();
 
+    let subscriptionCount = await database
+      .collection('users')
+      .aggregate([
+        { $unwind: { path: '$subscribed', preserveNullAndEmptyArrays: true } },
+        {
+          $project: {
+            _id: 0,
+            firstName: 0,
+            lastName: 0,
+            phoneNumber: 0,
+            email: 0,
+            role: 0,
+          },
+        },
+        {
+          $group: {
+            _id: '$subscribed.id',
+            totalCount: { $sum: 1 },
+            subCount: {
+              $sum: {
+                $cond: ['$subscribed.checkedIn', 0, 1],
+              },
+            },
+            checkedInCount: {
+              $sum: {
+                $cond: ['$subscribed.checkedIn', 1, 0],
+              },
+            },
+          },
+        },
+      ])
+      .toArray();
+
     eventsAndUserSubs.forEach((item) => {
+      // set if user is subscribed
       if (
         item.subscribed &&
         item.eventId ===
@@ -52,7 +87,20 @@ module.exports = async function (context, req) {
       }
     });
 
-    context.res = { body: filtered };
+    filtered.forEach((item) => {
+      // add sub count to event
+      let count = subscriptionCount.find((res) => {
+        return res._id === item.eventId;
+      });
+      filteredAndSubCount.push({
+        ...item,
+        totalCount: count.totalCount,
+        subCount: count.subCount,
+        checkedInCount: count.checkedInCount,
+      });
+    });
+
+    context.res = { body: filteredAndSubCount };
   } catch (error) {
     context.log(`Error code: ${error.code} message: ${error.message}`);
     context.res = {
